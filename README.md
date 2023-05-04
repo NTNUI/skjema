5e7f3f435701541d6bf21568ec0a276170aad594
Mats Finsås <12988459+Xtrah@users.noreply.github.com>
2023-05-04 16:44:02 +0200

    feat: use PyMuPDF to generate PDFs (#35)
    Rework generate pdf logic with the package PyMuPDF to support PDF, PNG, JPG and GIF files as attachments, including 16-bit depth PNGs.

--- README.md
+++ README.md
@@ -17,29 +17,29 @@ To run just the frontend:
 To run the backend/everything:
 
 - Make a virtual env with `python -m venv venv`
-- Enter the env with `source venv/bin/activate`
+- Enter the env with (Unix) `source venv/bin/activate` or (Windows) `source venv/Scripts/activate`
+- Make sure you are using latest pip with `python -m pip install --upgrade pip`
 - Install packages with `pip install -r kaaf/req.txt`
 - Start the server with `python kaaf/server.py`
 - If the frontend is exported (`yarn export`), the webapp will be available at `localhost:5000` when running `server.py`
-
-> One of the packages (pdf2image) will require poppler to work correctly with tmp files. Most linux distros come with this.
-> For MacOS `brew install poppler`
 
 ### Generating PDFs
 
 It might be nice to be able to quickly generate PDFs when developing, without having to start up everything. To do this you can run:
 
 ```python
-python kaaf/generate-example.py signature.png output.pdf image0.png image1.png ...
+python kaaf/generate-example.py signature.png attachment1.png attachment2.pdf ...
 ```
 
-Where `signature.png` and `imageN.png` are paths to image files (the latter images are optional)
+Where `signature.png` and `attachmentN.XYZ` are paths to image files.
 
 ## Environment variables
 
+While developing locally, you can temporarily add environment variables to the Dockerfile, such as `ENV MAIL_ADDRESS="no-reply@ntnui.no"`, or by creating an **.env** file with `KEY=VALUE` pairs separated by a newline.
+
 | Variable        | Function                                     |
 | --------------- | -------------------------------------------- |
-| `MAIL_ADDRESS`  | Set the mail address for generated receipts |
-| `MAIL_PASSWORD` | Password for the mail account                |
+| `SERVICE_ACCOUNT_STR` | Google service account string          |
+| `MAIL_ADDRESS`  | Set the mail address for generated receipts  |
 | `ENVIRONMENT`   | Set to "production" for sentry errors        |
 | `SENTRY_DSN`    | Ingest errors to sentry                      |
--- kaaf/generate-example.py
+++ kaaf/generate-example.py
@@ -1,49 +1,78 @@
 import argparse
 import base64
+import sys
+import os
+import magic
 
-from handler import create_pdf, modify_data
+from handler import create_pdf
 
-default_data = {
-    "date": "2020-12-27",
-    "amount": "69 kr",
-    "name": "Mats",
-    "accountNumber": "010101010101",
-    "committee": "Hovedstyret",
-    "occasion": "Teste litt",
-    "comment": "pls work",
+test_data = {
+    "name": "John Doe",
+    "mailfrom": "johndoe@ntnui.dev",
+    "committee": "Sprint",
+    "accountNumber": "123456789",
+    "amount": "69.69",
+    "date": "2023-05-17",
+    "occasion": "Expense reimbursement",
+    "comment": "Some comment\n with multiple\n newlines",
 }
 
+if len(sys.argv) < 3:
+    print("Error: Missing arguments")
+    print(f"Usage: python3 {sys.argv[0]} signature_file, attachment_files")
+    print(f"Output: output.pdf")
+    sys.exit(1)
 
-def main(data, out):
-    data = modify_data(data)
+# Parse the command line arguments
+signature_file = sys.argv[1]
+attachment_files = sys.argv[2:]
 
-    pdf = create_pdf(data)
-
-    with open(out, "wb") as f:
-        f.write(pdf.encode("latin-1"))
-
-    print("Done!")
-
-
-def encode_image(img):
-    with open(img, "rb") as f:
-        b64 = base64.b64encode(f.read()).decode("ascii")
-    return f'data:image/{img.split(".")[-1]};base64,{b64}'
+allowed_extensions = {".pdf", ".jpg", ".jpeg", ".png", ".gif", ".heic"}
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("signature", help="Path to signature")
-    parser.add_argument("out", help="Path to the generated pdf")
-    parser.add_argument(
-        "images", nargs=argparse.REMAINDER, default=[], help="Paths to images"
-    )
-    args = parser.parse_args()
+def is_valid_file_extension(file_path, allowed_extensions):
+    _, file_extension = os.path.splitext(file_path)
+    return file_extension.lower() in allowed_extensions
 
-    data = {
-        **default_data,
-        "signature": encode_image(args.signature),
-        "images": [encode_image(img) for img in args.images],
-    }
 
-    main(data, args.out)
+# Return exception if signature or attachment files are not valid
+if not is_valid_file_extension(signature_file, allowed_extensions):
+    raise Exception(f"Invalid signature file extension: {signature_file}")
+for file_path in attachment_files:
+    if not is_valid_file_extension(file_path, allowed_extensions):
+        raise Exception(f"Invalid attachment file extension: {file_path}")
+
+# Convert signature file to base64:image/png
+with open(signature_file, "rb") as f:
+    signature = f.read()
+    signature = base64.b64encode(signature).decode("utf-8")
+    signature = f"data:image/png;base64,{signature}"
+
+# Check file type and convert attachment files to base64 with MIME type
+attachments = []
+for file_path in attachment_files:
+    with open(file_path, "rb") as f:
+        # Read the file as bytes
+        file_data = f.read()
+
+        # Detect the filetype using python-magic
+        file_type = magic.from_buffer(file_data, mime=True)
+
+        # Convert the file data to base64
+        file_data = base64.b64encode(file_data).decode("utf-8")
+
+        # Add the filetype prefix to the base64 string
+        file_data = f"data:{file_type};base64,{file_data}"
+
+        # Check if the filetype is one of the allowed ones
+        allowed_types = ["application/pdf", "image/jpeg", "image/png", "image/heic"]
+        if file_type in allowed_types:
+            # Append the file data to the attachments list
+            attachments.append(file_data)
+
+print(f"Signature: {signature[:50]}")
+for attachment in attachments:
+    print(f"Attachment: {attachment[:50]}")
+
+# Call the create_pdf function to generate the PDF
+create_pdf(test_data, signature, attachments)
